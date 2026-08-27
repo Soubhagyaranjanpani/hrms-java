2c3943c19a21a157d03b486af43f90c6e57060f5
Ritu Kushwaha <reetukushwah50@gmail.com>
2026-08-27 13:01:12 +0530

    remove  file

---
+++ src/main/java/com/hrms/audit/domain/AuditModule.java
@@ -0,0 +1,20 @@
+package com.hrms.audit.domain;
+
+public final class AuditModule {
+    public static final String APPOINTMENT   = "Appointment";
+    public static final String PROMOTION     = "Promotion";
+    public static final String TRANSFER      = "Transfer";
+    public static final String EMPLOYEE      = "Employee";
+    public static final String PAY_REVISION  = "Pay Revision";
+    public static final String DOCUMENTS     = "Documents";
+    public static final String SERVICE_BOOK  = "Service Book";   // 👈 requested module
+    public static final String LEAVE         = "Leave";
+    public static final String CONFIRMATION  = "Confirmation";
+    public static final String TRAINING      = "Training";
+    public static final String AWARD         = "Award";
+    public static final String DISCIPLINARY  = "Disciplinary";
+    public static final String DEPUTATION    = "Deputation";
+    public static final String RETIREMENT    = "Retirement";
+
+    private AuditModule() {}
+}
